c4f116249b2e2bc088c769105e610f655df4517c
NeosnakeEX <andrew.rmz13@gmail.com>
2020-08-26 13:15:14 -0400

    Finished both functions and added comments explaining them.

--- cachematrix.R
+++ cachematrix.R
@@ -1,42 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeVector <- function(x = numeric()) {
-  m <- NULL
-  set <- function(y) {
-    x <<- y
-    m <<- NULL
+## MakeCacheMatrix contains 4 functions, a set function that can change the matrix that was originally inputted,
+## a get function that returns the matrix x, a setinverse function that sets the inverse of the matrix x, and 
+## a getinverse function that returns the inverse matrix. The makeCacheMatrix returns a list containing these
+## four functions.
+makeCacheMatrix <- function(x = matrix()) {
+  inverse <- NULL
+  set <- function(matrix) {
+    x <<- matrix
+    inverse <<- NULL
   }
   get <- function() x
-  setmean <- function(mean) m <<- mean
-  getmean <- function() m
-  list(set = set, get = get,
-       setmean = setmean,
-       getmean = getmean)
-}
-
-##
-cachemean <- function(x, ...) {
-  m <- x$getmean()
-  if(!is.null(m)) {
-    message("getting cached data")
-    return(m)
-  }
-  data <- x$get()
-  m <- mean(data, ...)
-  x$setmean(m)
-  m
-}
-
-makeCacheMatrix <- function(x = matrix()) {
-
+  setinverse <- function(newinverse) inverse <<- newinverse
+  getinverse <- function() inverse
+  list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## CacheSolve takes in a special 'matrix' object. It first tries to find if a inverse has been calculated and if
+## it has, to return it. If not, it will fetch the original input matrix from makeChacheMatrix and will find the
+## inverse of it using the solve function. It will then assign the set inverse function in the special 'matrix' 
+## object to the calculated inverse and it will return the inverse.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getinverse()
+  if(is.null(inverse) == FALSE) {
+    message('Getting cached matrix')
+    return(inverse)
+  }
+  getmatrix <- x$get()
+  calculated_inverse <- solve(getmatrix)
+  x$setinverse(calculated_inverse)
+  calculated_inverse
+        
 }
